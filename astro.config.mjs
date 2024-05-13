eeddc9bc769e6bcd9adc372d2366949d735c6c31
AyagiKei <kei.ayagi@gmail.com>
2024-05-14 00:13:12 +0800

    feat: update project

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,7 +31,7 @@ const oklchToHex = (str) => {
 // https://astro.build/config
 export default defineConfig({
   site: "https://aronaplana.io/",
-  base: "/",
+  // base: "/",
   trailingSlash: "always",
   integrations: [
     tailwind(),
